bd048863dc4fefa363af140e22b77a2603cb672c
AlekseiMakhov <66823049+AlekseiMakhov@users.noreply.github.com>
2021-01-10 19:47:07 +0300

    dev: fix header texts

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header({ isMobile, email, loggedIn, isSignInLocation, setSignInScreen, 
                     <img className="header__logo" src={logo} alt="Лого" />
                     <p className="header__text">{email}</p>
                     {loggedIn && <Link className="header__text" onClick={handleSignOut} to='/signin'>Выйти</Link>}
-                    {!loggedIn && isSignInLocation && <Link className="header__text" onClick={resetSignInScreen} to='/signup'>Зарегистрироваться</Link>}
+                    {!loggedIn && isSignInLocation && <Link className="header__text" onClick={resetSignInScreen} to='/signup'>Регистрация</Link>}
                     {!loggedIn && !isSignInLocation && <Link className="header__text" onClick={setSignInScreen} to='/signin'>Войти</Link>}
                 </header>
             
@@ -18,7 +18,7 @@ function Header({ isMobile, email, loggedIn, isSignInLocation, setSignInScreen, 
                     <img className="header-mobile__logo" src={logo} alt="Лого" />
                     {isExpanded && <p className="header-mobile__text header-mobile__text_type_email">{email}</p>}
                     {loggedIn && isExpanded && <Link className={cn('header-mobile__text', { 'header-mobile__text_type_signout': isExpanded })} onClick={handleSignOut} to='/signin'>Выйти</Link>}
-                    {!loggedIn && isSignInLocation && <Link className="header-mobile__text" onClick={resetSignInScreen} to='/signup'>Зарегистрироваться</Link>}
+                    {!loggedIn && isSignInLocation && <Link className="header-mobile__text" onClick={resetSignInScreen} to='/signup'>Регистрация</Link>}
                     {!loggedIn && !isSignInLocation && <Link className="header-mobile__text" onClick={setSignInScreen} to='/signin'>Войти</Link>}
                     {isExpanded && <div className='header-mobile__line'></div>}
                     {loggedIn && <button className={cn('header-mobile__menu-button', { 'header-mobile__menu-button_type_close': isExpanded })} onClick={expand}></button>}
